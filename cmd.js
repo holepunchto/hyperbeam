cc77879e91ff35dda430c985e3a14b9883aac1cb
rafapaezbas <rafa@holepunch.com>
2024-03-20 12:44:28 +0100

    bare integration

---
+++ cmd.js
@@ -0,0 +1,67 @@
+const Hyperbeam = require('./')
+
+if (global.Bare) process = require('bare-process') // eslint-disable-line
+
+module.exports = (argv) => {
+  if (argv.includes('-h') || argv.includes('--help')) {
+    console.error('Usage: hyperbeam [passphrase]')
+    console.error('')
+    console.error('  Creates a 1-1 end-to-end encrypted network pipe.')
+    console.error('  If a passphrase is not supplied, will create a new phrase and begin listening.')
+    process.exit(1)
+  }
+
+  let beam
+  try {
+    beam = new Hyperbeam(argv[2], argv.includes('-r'))
+  } catch (e) {
+    if (e.constructor.name === 'PassphraseError') {
+      console.error(e.message)
+      console.error('(If you are attempting to create a new pipe, do not provide a phrase and hyperbeam will generate one for you.)')
+      process.exit(1)
+    } else {
+      throw e
+    }
+  }
+
+  if (beam.announce) {
+    console.error('[hyperbeam] Run hyperbeam ' + beam.key + ' to connect')
+    console.error('[hyperbeam] To restart this side of the pipe with the same key add -r to the above')
+  } else {
+    console.error('[hyperbeam] Connecting pipe...')
+  }
+
+  beam.on('remote-address', function ({ host, port }) {
+    if (!host) console.error('[hyperbeam] Could not detect remote address')
+    else console.error('[hyperbeam] Joined the DHT - remote address is ' + host + ':' + port)
+  })
+
+  beam.on('connected', function () {
+    console.error('[hyperbeam] Success! Encrypted tunnel established to remote peer')
+  })
+
+  beam.on('error', function (e) {
+    console.error('[hyperbeam] Error:', e.message)
+    closeASAP()
+  })
+
+  beam.on('end', () => beam.end())
+
+  process.stdin.pipe(beam).pipe(process.stdout)
+  if (typeof process.stdin.unref === 'function') process.stdin.unref()
+
+  process.once('SIGINT', () => {
+    if (!beam.connected) closeASAP()
+    else beam.end()
+  })
+
+  function closeASAP () {
+    console.error('[hyperbeam] Shutting down beam...')
+
+    const timeout = setTimeout(() => process.exit(1), 2000)
+    beam.destroy()
+    beam.on('close', function () {
+      clearTimeout(timeout)
+    })
+  }
+}
